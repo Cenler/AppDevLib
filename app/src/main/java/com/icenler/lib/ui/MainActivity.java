1a60b90cbb79d48bb1c6acd34343cc868d77933b
yangchenglong <yangchenglong@corp.vipshop.com>
2015-09-30 18:14:09 +0800

    Paletter 使用及颜色变换

--- app/src/main/java/com/icenler/lib/ui/MainActivity.java
+++ app/src/main/java/com/icenler/lib/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.icenler.lib.ui;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.NavigationView;
@@ -191,6 +192,32 @@ public class MainActivity extends BaseCompatActivity {
         public CharSequence getPageTitle(int position) {
             return tabTitle.get(position);
         }
+    }
+
+    /**
+     * 颜色加深处理
+     *
+     * @param RGBValues RGB的值，由alpha（透明度）、red（红）、green（绿）、blue（蓝）构成，
+     *                  Android中我们一般使用它的16进制，
+     *                  例如："#FFAABBCC",最左边到最右每两个字母就是代表alpha（透明度）、
+     *                  red（红）、green（绿）、blue（蓝）。每种颜色值占一个字节(8位)，值域0~255
+     *                  所以下面使用移位的方法可以得到每种颜色的值，然后每种颜色值减小一下，在合成RGB颜色，颜色就会看起来深一些了
+     *                  Palette.Builder from = Palette.from(bitmap);
+     *                  Palette generate = from.generate();
+     *                  generate.getDarkMutedSwatch().getRgb();
+     * @return
+     */
+    private int colorBurn(int RGBValues) {
+        int alpha = RGBValues >> 24;
+        int red = RGBValues >> 16 & 0xFF;
+        int green = RGBValues >> 8 & 0xFF;
+        int blue = RGBValues & 0xFF;
+
+        red = (int) Math.floor(red * (1 - 0.1));
+        green = (int) Math.floor(green * (1 - 0.1));
+        blue = (int) Math.floor(blue * (1 - 0.1));
+
+        return Color.rgb(red, green, blue);
     }
 
 }
