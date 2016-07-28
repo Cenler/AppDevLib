8109b5856e50e29eb76c5619d3b20c84f8f077dc
fangde <fangde@b5m.com>
2016-07-28 15:40:20 +0800

    commit

--- app/src/main/java/com/icenler/lib/feature/mvp/base/BasePresenter.java
+++ app/src/main/java/com/icenler/lib/feature/mvp/base/BasePresenter.java
@@ -4,24 +4,26 @@ import android.support.annotation.NonNull;
 
 import com.icenler.lib.utils.Preconditions;
 
+import rx.Subscription;
 import rx.subscriptions.CompositeSubscription;
 
 public class BasePresenter<V extends BaseView> implements IPresenter<V> {
 
-    protected V mViewHandle;
-    protected CompositeSubscription mSubscriptions;
+    private V mViewHandle;
+    private CompositeSubscription mSubscriptions;
 
     @Override
     public void attachView(@NonNull V viewHandle) {
         mViewHandle = Preconditions.checkNotNull(viewHandle);
-        mSubscriptions = new CompositeSubscription();
     }
 
     @Override
     public void detachView() {
         mViewHandle = null;
-        mSubscriptions.unsubscribe();
-        mSubscriptions = null;
+        if (mSubscriptions != null) {
+            mSubscriptions.unsubscribe();
+            mSubscriptions = null;
+        }
     }
 
     public boolean isAttached() {
@@ -30,6 +32,13 @@ public class BasePresenter<V extends BaseView> implements IPresenter<V> {
 
     public V getViewHandle() {
         return mViewHandle;
+    }
+
+    public void addSubscription(Subscription s) {
+        if (mSubscriptions == null) {
+            mSubscriptions = new CompositeSubscription();
+        }
+        mSubscriptions.add(s);
     }
 
 }
